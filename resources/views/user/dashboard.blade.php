577c1803089fea964c7f757a8de4e5f2a8255619
wokesdev <khoandy752@gmail.com>
2021-05-05 16:02:34 +0700

    Update - 5 May 2021

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Dashboard')
 @section('content')
 <div class="content">
-    <div class="panel-header bg-primary-gradient"><div class="page-inner py-5"><div class="d-flex align-items-left align-items-md-center flex-column flex-md-row"><div><h2 class="text-white pb-2 fw-bold">Dashboard</h2><h5 class="text-white op-7 mb-2">Sistem Akuntansi - Salon Fas</h5></div></div></div></div>
+    <div class="panel-header bg-primary-gradient"><div class="page-inner py-5"><div class="d-flex align-items-left align-items-md-center flex-column flex-md-row"><div><h2 class="text-white pb-2 fw-bold">Dashboard</h2><h5 class="text-white op-7 mb-2">Sistem Akuntansi - {{ config('app.name') }}</h5></div></div></div></div>
     <div class="page-inner mt--5">
         <div class="row mt--2">
             <div class="col-md-12">
